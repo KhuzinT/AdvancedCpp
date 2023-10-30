da76d84b5ce32f0ccc47d37494d521a39f1bde86
Khuzin Timur <KhuzinTka@yandex.ru>
2023-10-30 22:26:17 +0300

    hw3: best-time-to-buy-ans-sell-stock

--- hw3/README.md
+++ hw3/README.md
@@ -2,7 +2,8 @@
 
 # [Best-time-to-buy-and-sell-stock](https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/)
 
-
+Отслеживаем минимальную цену в уже прочитанном массиве и потенциальную прибыль, если мы продадим акцию по текущей цене.
+Выбираем максимальную потенциальную прибыль
 
 # [Single-number-iii](https://leetcode.com/problems/single-number-iii/description/)
 
--- hw3/best-time-to-buy-and-sell-stock.cpp
+++ hw3/best-time-to-buy-and-sell-stock.cpp
@@ -1,12 +1,50 @@
 #include <iostream>
 #include <cassert>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
+
+    int maxProfit(vector<int>& prices) {
+        // нет акций - нет прибыли
+        if (prices.empty()) {
+            return 0;
+        }
+
+        // хотим найти минимальную цену
+        int min_price = prices[0];
+
+        int res = 0;
+        for (int idx = 1; idx < prices.size(); ++idx) {
+            // профит - это максимальная разница между текущей ценой и минимальной, которая была до нее
+            res = max(res, prices[idx] - min_price);
+            min_price = min(min_price, prices[idx]);
+        }
+
+        return res;
+    }
 };
 
 int main() {
+    auto sol = Solution();
 
+    {
+        vector prices = {7, 1, 5, 3, 6, 4};
+        auto output = sol.maxProfit(prices);
+
+        assert(output == 5);
+
+        std::cout << "Test 1 passed\n";
+    }
+
+    {
+        vector prices = {7, 6, 4, 3, 1};
+        auto output = sol.maxProfit(prices);
+
+        assert(output == 0);
+
+        std::cout << "Test 2 passed\n";
+    }
 }
